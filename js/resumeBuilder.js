261cef868ba9a4053fa3bce3b6be6924cce9ff76
Javed Qadrud-Din <jqjunk@MacBook-Pro-2.home>
2016-01-05 16:47:06 -0500

    at end of lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,106 @@
+//$("#main").append("Javed Qadrud-Din");
+
+//var awesomeThoughts = "I am Javed and I am AWESOME";
+//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+//$("#main").append(funThoughts);
+
+
+
+
+
+var bio = {
+	"name" : "Javed Qadrud-Din",
+	"role" : "Business Architect",
+	"contactInfo" : {
+		"e-mail": "javed.qadruddin@gmail.com",
+		"phone": "203.536.9649"
+	},
+	"pic" : "images/myPic.jpg",
+	"message" : "Recovering lawyer, awesome technologizer.",
+	"skills" : [
+		"Javascript", 
+		"Python", 
+		"Objective-C", 
+		"iOS", 
+		"Java", 
+		"Law of startups, venture capital and mergers and acquisitions"
+	]
+};
+
+var work = {
+	"jobs": [
+		{
+			"company": "IBM",
+			"position": "Business Architect",
+			"dateStarted": "March, 2015",
+			"dateEnded": "Present"
+		},
+		{
+			"company": "IBM",
+			"position": "Software Engineer",
+			"dateStarted": "June, 2014",
+			"dateEnded": "March, 2015"
+		},
+		{
+			"company": "Fenwick & West, LLP",
+			"position": "Associate Attorney",
+			"dateStarted": "October, 2011",
+			"dateEnded": "July 2013"
+		}
+	]
+};
+
+work.employer = "IBM";
+work.yearsWorked = "1.5";
+work.city = "San Jose";
+
+var education = {
+	"schools": [
+		{
+			"name": "Harvard Law School",
+			"city": "Cambridge, MA",
+			"degree": "JD",
+			"major": ["Law"]
+		},
+		{
+			"name": "Cornell University",
+			"city": "Ithaca, NY",
+			"degree": "BA",
+			"major": ["History","Film"]
+		}
+	]
+};
+
+//education["school"] = "Harvard Law School";
+//education["years"] = "2008-2011";
+//education["city"] = "Cambridge, Massachusetts";
+
+
+//var formattedName = HTMLheaderName.replace("%data%", bio.name);
+//var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+//var formattedEmail = HTMLemail.replace("%data%", bio.contactInfo);
+//var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.message);
+//var formattedPic = HTMLbioPic.replace("%data%", bio.pic);
+//var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+//var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.employer);
+//var formattedEducationName = HTMLschoolName.replace("%data%", education.school);
+
+
+//$("#main").prepend(formattedWorkEmployer);
+
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+//$("#lets-connect").append(formattedEmail);
+//$(".welcome-message").append(formattedMessage);
+//$(".biopic").append(formattedPic);
+//$("#skills-h3").append(HTMLskillsStart);
+//$("#skills-h3").append(formattedSkills);
+//$("#workExperience").append(formattedWorkEmployer);
+//$("#education").append(formattedEducationName);
+
+
+
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
